e1b69b917456e94b715ee1de48dc121ad4339566
Bracken <gabrielcache2226@gmail.com>
2025-04-23 18:04:00 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab5.js - Data Types and Variables
+// Author: Gabriel Specht
+// Date: 04/23/25
 
-// Constants
+let make = "Toyota";
 
-// Functions
+let model = "Coaster";
+
+let color = "blue";
+
+let year = 1969;
+
+let ownIt = false;
+
+let age = 2025 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
