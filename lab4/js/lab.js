aa84c0c9ec7542fce6ae7a8a5ed364fbf8052f22
Bracken <gabrielcache2226@gmail.com>
2025-04-23 18:27:19 -0700

    lab5 update

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock-Paper-Scissors
+// Author: Gabriel Specht
+// Date: April 21, 2025
 
-// Constants
-
-// Functions
+//Assign players to either designations "Player 1" or "Player 2"
+//When round starts, provide 3 available inputs (rock, paper, scissors) for the first player
+//Prompt them to select an input
+//After both players have selected an input, decide if a player has reached a winning instance
+  //Winning instances: (rock beats scissors, scissors beats paper, paper beats rock)
+//If both players play the same input, the round is a draw, and no one gets points. The round starts over
+//The player that has reached the winning instance gets a point
+//First player to reach two points wins the game
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
