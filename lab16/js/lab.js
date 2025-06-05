600b3052596266f44aff13ca205fb01f29bdc53e
Bracken <gabrielcache2226@gmail.com>
2025-06-04 19:55:34 -0700

    lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,25 +2,22 @@
 // Author: Gabriel Specht
 // Date: 6/4/2025
 
-
+// API
 let comicObj = {
-    url: "https://xkcd.com",
-    data:{
-        id: 614,
-    },
+    url: "https://xkcd.com/" + Math.floor(Math.random() * 3098) + "/info.0.json",
     type: "GET",
     dataType: "JSON"
 }
 
-
+// Accesses API
 $.ajax(comicObj)
 
+//assigns variables and displays them
 .done(function(data){
     console.log(data);
 
-    let title = data.safe_title;
+    let comicTitle = data.title;
     let imgURL = data.img;
     let alt = data.alt;
-
-    $("#output").html("<h1>" + title + "<div>" + `<img src='${imgURL}'/>`);
+$("#output").html("<h1>" + comicTitle + "<div>" + "<img src='" + imgURL + "'/>" + "<div>" + "<p>" + alt);
 })
